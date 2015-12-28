525d2adaab2b84b489ccca3095127fdb2f7db786
volkeee <volkeee1@gmail.com>
2015-12-28 23:43:40 +0200

    Friend adapter, items for lists

---
+++ app/src/main/java/vne/vkmusic/adapters/FriendsListAdapter.java
@@ -0,0 +1,45 @@
+package vne.vkmusic.adapters;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.ArrayList;
+
+import vne.vkmusic.audio.VkAudio;
+import vne.vkmusic.friend.Friend;
+
+/**
+ * Created by volke on 28.12.2015.
+ */
+public class FriendsListAdapter extends BaseAdapter{
+    private ArrayList<Friend> listData;
+    private LayoutInflater layoutInflater;
+
+    public FriendsListAdapter(Context aContext, ArrayList<Friend> listData) {
+        this.listData = listData;
+        layoutInflater = LayoutInflater.from(aContext);
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
